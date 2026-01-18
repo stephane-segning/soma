bf335a92c0eea435cc8e8ba9bed9578118a8cb91
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2026-01-18 16:54:50 +0100

    feat: update package versions and enhance build configuration for Electron apps

--- desktop/tapia/electron.vite.config.ts
+++ desktop/tapia/electron.vite.config.ts
@@ -1,16 +1,59 @@
+import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
+import VitePluginCssMediaSplitter from "css-media-splitter/vite-plugin";
 import { defineConfig } from "electron-vite";
-import { resolve } from "path";
+import type { PreRenderedAsset } from "rollup";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
+import topLevelAwait from "vite-plugin-top-level-await";
+import { vitePluginVersionMark } from "vite-plugin-version-mark";
+import wasm from "vite-plugin-wasm";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig((configEnv) => ({
 	main: {},
 	preload: {},
 	renderer: {
 		resolve: {
-			alias: {
-				"@renderer": resolve("src/renderer/src"),
+			alias: [{ find: "@app", replacement: resolve("src/renderer/src") }],
+		},
+		plugins: [
+			react(),
+			tsconfigPaths(),
+			configEnv.command === "build" &&
+				ViteImageOptimizer({
+					/* pass your config */
+				}),
+			configEnv.command === "build" && VitePluginCssMediaSplitter(),
+			configEnv.command === "build" &&
+				vitePluginVersionMark({
+					ifShortSHA: true,
+					ifMeta: true,
+					ifLog: true,
+					ifGlobal: true,
+				}),
+			configEnv.command === "build" && ViteMinifyPlugin({}),
+			wasm(),
+			topLevelAwait(),
+		],
+		build: {
+			chunkSizeWarningLimit: 5_000, // 5KB
+			rollupOptions: {
+				output: {
+					entryFileNames: "assets/js/[name]-[hash].js",
+					chunkFileNames: "assets/js/chunks/[name]-[hash].js",
+					assetFileNames: (assetInfo: PreRenderedAsset) => {
+						if (
+							assetInfo.names?.some((name) => name?.endsWith(".css")) ??
+							assetInfo.name?.endsWith(".css")
+						) {
+							return "assets/css/[name]-[hash][extname]";
+						}
+
+						return "assets/[name]-[hash][extname]";
+					},
+				},
 			},
 		},
-		plugins: [react()],
 	},
-});
+}));
